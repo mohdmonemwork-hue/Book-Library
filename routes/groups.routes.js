3922938379ab7405c9bb411276aa8b332d1f6f60
mohdmonem <mohdmonem.work@gmail.com>
2026-09-23 10:46:48 +0300

    Removed the admin functionality from Groups to create admin dashboard

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,24 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Group = require("../models/Group.js");
-const User = require("../models/User.js");
 const Book = require("../models/Book.js");
 const isSignedIn = require("../middleware/is-signed-in.js");
-const isSuperAdmin = require("../middleware/is-super-admin.js");
-
-router.get("/new", isSignedIn, isSuperAdmin, (req, res) => {
-  res.render("groups/new.ejs");
-});
-
-router.post("/", isSignedIn, isSuperAdmin, async (req, res) => {
-  await Group.create({
-    name: req.body.name,
-    description: req.body.description,
-    createdBy: req.session.user._id,
-    members: [],
-  });
-  res.redirect("/groups");
-});
 
 //admin see groups
 router.get("/", isSignedIn, async (req, res) => {
@@ -55,28 +39,6 @@ router.get("/:groupId", isSignedIn, async (req, res) => {
   });
   const books = await Book.find().where("owner").in(memberIds);
   res.render("groups/show.ejs", { group, books });
-});
-
-router.post("/:groupId/members", isSignedIn, isSuperAdmin, async (req, res) => {
-  const group = await Group.findById(req.params.groupId);
-
-  if (!group) {
-    return res.redirect("/groups");
-  }
-
-  const userToAdd = await User.findOne({ username: req.body.username });
-  if (!userToAdd) {
-    return res.send("User not found.");
-  }
-
-  const alreadyMember = group.members.some((id) => id.equals(userToAdd._id));
-  if (alreadyMember) {
-    return res.send("User is already in this group.");
-  }
-
-  group.members.push(userToAdd._id);
-  await group.save();
-  res.redirect(`/groups/${group._id}`);
 });
 
 module.exports = router;
