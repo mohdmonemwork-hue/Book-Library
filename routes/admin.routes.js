897d140a59b6c3ded8466dfae281433660fcdacb
mohdmonem <mohdmonem.work@gmail.com>
2026-09-23 18:59:11 +0300

    added group member edit route

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -20,4 +20,47 @@ router.get("/groups/new", isSignedIn, isSuperAdmin, (req, res) => {
   res.render("admin/CreateGroup.ejs");
 });
 
+router.post("/groups", isSignedIn, isSuperAdmin, async (req, res) => {
+  await Group.create({
+    name: req.body.name,
+    description: req.body.description,
+    createdBy: req.session.user._id,
+    members: [],
+  });
+  res.redirect("/admin");
+});
+
+router.get(
+  "/groups/:groupId/edit",
+  isSignedIn,
+  isSuperAdmin,
+  async (req, res) => {
+    const group = await Group.findById(req.params.groupId).populate("members");
+
+    res.render("admin/editMemebers.ejs", { group });
+  },
+);
+
+router.post(
+  "/groups/:groupId/members",
+  isSignedIn,
+  isSuperAdmin,
+  async (req, res) => {
+    const group = await Group.findById(req.params.groupId);
+
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.send("User not found.");
+
+    const alreadyMember = group.members.some((id) => {
+      return id.toString() === user._id.toString();
+    });
+
+    if (!alreadyMember) {
+      group.members.push(user._id);
+      await group.save();
+    }
+
+    res.redirect(`/admin/groups/${group._id}/edit`);
+  },
+);
 module.exports = router;
